1a72e569163f7d7cfbc3838d453c39e9ef7f0cc6
Vrushali Patil <139788628+Vrusha1i@users.noreply.github.com>
2023-12-23 22:55:00 +0530

    check character is vowel or consonent

---
+++ Result.java
@@ -0,0 +1,19 @@
+package checkCharacter.com;
+
+import java.util.Scanner;
+
+public class Result {
+
+	public static void main(String[] args) {
+
+		char ch='e';
+		if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+			System.out.println(ch+" is vowel");
+		}
+		else
+		{
+			System.out.println("character is consonent");
+		}
+	}
+
+}
