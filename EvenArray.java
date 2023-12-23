7dcec70c0b223892a848f43333fece2a6f753c52
Vrushali Patil <139788628+Vrusha1i@users.noreply.github.com>
2023-12-23 22:47:34 +0530

    Add files via upload

---
+++ EvenArray.java
@@ -0,0 +1,18 @@
+package arrayonEvenPosition.com;
+
+public class EvenArray {
+
+	public static void main(String[] args) {
+
+	int arr[]= {1,4,6,8,4,2,6,8,9};
+	
+
+	
+	for(int i=1;i<arr.length;i=i+2) {
+		
+			System.out.print(arr[i]+" ");
+		
+	}
+	}
+
+}
