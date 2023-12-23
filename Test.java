a0086542008a36d5c4a47b35fe9ee3e27a6e0155
Vrushali Patil <139788628+Vrusha1i@users.noreply.github.com>
2023-12-23 22:41:12 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,33 @@
+package armstrongNumber.com;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	Scanner sc=new  Scanner(System.in);
+	System.out.println("enter any number");
+	int a=sc.nextInt();
+	int r;
+	int sum=0;
+	int a1=a;
+	
+	while(a>0) {
+		r=a%10;
+		sum=sum+(r*r*r);
+		a=a/10;
+		
+	}
+	if(sum==a1) {
+		System.out.println("Given number is armstrong number");
+	}
+
+	else
+	{
+		System.out.println("Given number is not a armstrong number");
+	}
+	
+	}
+
+}
