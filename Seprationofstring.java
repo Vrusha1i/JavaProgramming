e2eea97f895c4f67b1745c23c3b134f063d92e7a
Vrushali Patil <139788628+Vrusha1i@users.noreply.github.com>
2023-12-29 22:25:09 +0530

    Sepration of string into char

---
+++ Seprationofstring.java
@@ -0,0 +1,14 @@
+package seperationOfCharFromString;
+
+public class Seprationofstring {
+
+	public static void main(String[] args) {
+
+		String str="Vrushali";
+		
+		for(int i=0;i<str.length();i++) {
+			System.out.print(str.charAt(i)+" "); 
+		}
+	}
+
+}
